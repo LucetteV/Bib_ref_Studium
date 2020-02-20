9ece488d15044b23465d805ab256f46168f8f7c3
LaMOP <stephane.lamasse@univ-paris1.fr>
2020-02-20 13:27:42 +0100

    moficiation des fichiers du dossier test

---
+++ Aegidius_romanus_test/csv_to_json.py
@@ -0,0 +1,10 @@
+import csv
+import json
+
+csv_file = open('aegidius_bibliography.csv', 'r')
+json_file = open('aegidius_bibliography.json', 'w')
+
+rowfields = ("id", "Author_name", "Titre_Article", "Periodic_title", "Print_Date", "pages")
+reader = csv.DictReader(csv_file, rowfields)
+out = json.dumps([row for row in reader])
+json_file.write(out)
